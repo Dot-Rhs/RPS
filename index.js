982984ca4cf5606090f6cfd6591f1584e3381b4d
Dot-rhs <rhysh1986@gmail.com>
2019-06-17 00:30:15 +0100

    added scores, reset, html

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 console.log("hi");
 
 const title = "Rock, Paper, Scissors?";
+var playerScore = 0;
+var compScore = 0;
+
+plyScr = document.getElementById("playerScore");
+cmpScr = document.getElementById("computerScore");
 
 function playGame(playerMove) {
   const ranNum = getRandomInt();
   var computerMove = getRandom(ranNum);
-  const result = compareMove(playerMove, computerMove);
-  console.log(result);
+  const result = compareMove(playerMove, computerMove, playerScore, compScore);
+  scoreCard(result);
+  console.log(`player: ${playerMove} computer: ${computerMove}`);
+  console.log(`result: ${result}`);
+  console.log(`Player Score: ${playerScore} Computer Score: ${compScore} `);
 }
 
 function getRandomInt() {
@@ -25,30 +33,39 @@ function getRandom(x) {
 
 function compareMove(playerMove, computerMove) {
   if (playerMove === computerMove) {
-    return "it's a draw";
-  } else if (playerMove === "scissors") {
-    if (computerMove === "rock") {
-      return "Computer Wins!";
-    } else if (computerMove === "paper") {
-      return "Player Wins!";
-    }
-  } else if (playerMove === "rock") {
-    if (computerMove === "paper") {
-      return "Computer Wins!";
-    } else if (computerMove === "scissors") {
-      return "Player Wins!";
-    }
-  } else if (playerMove === "paper") {
-    if (computerMove === "rock") {
-      return "player Wins!";
-    } else if (computerMove === "scissors") {
-      return "Computer Wins!";
-    }
+    alert("it's a draw");
+  } else if (
+    (playerMove === "scissors" && computerMove === "rock") ||
+    (playerMove === "rock" && computerMove === "paper") ||
+    (playerMove === "paper" && computerMove === "scissors")
+  ) {
+    alert("Computer Wins!");
+    return 1;
+    // compScore++;
   } else {
-    console.error("Pick one of those 3 smart ass!");
+    alert("Player Wins!");
+    return 2;
+    // playerScore++;
+    // console.error("Pick one of those 3 smart ass!");
   }
 }
 
+function scoreCard(result) {
+  if (result === 1) {
+    compScore++;
+    cmpScr.innerHTML = compScore;
+  } else if (result === 2) {
+    playerScore++;
+    plyScr.innerHTML = playerScore;
+  }
+}
+
+function clearScore() {
+  playerScore = 0;
+  plyScr.innerHTML = playerScore;
+  compScore = 0;
+  cmpScr = compScore;
+}
 // Old code
 /*if (playerMove === "rock" && computerMove === "paper") {
     console.log("Computer Wins!");
